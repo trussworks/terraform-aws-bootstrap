4b8d722d5cbb53eb8c5d17cf10042c0a25be607a
jeremy avnet <brainsik@truss.works>
2018-01-24 14:14:08 -0800

    Create DynamoDB table for state locking
    [#154579100]

--- main.tf
+++ main.tf
@@ -6,3 +6,22 @@ module "terraform-state-bucket" {
   version = "~> 1.3"
   bucket  = "terraform-state-${var.region}"
 }
+
+# Create Terraform locking table
+# ------------------------------
+
+resource "aws_dynamodb_table" "terraform-state-lock" {
+  name           = "terraform-state-lock"
+  hash_key       = "LockID"
+  read_capacity  = 5
+  write_capacity = 5
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags {
+    Name = "terraform-state-lock"
+  }
+}
