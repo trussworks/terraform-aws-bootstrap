98230ff845078bd15e0e7b5998549659b35acac2
Chikara <chtakahashi10@gmail.com>
2021-10-15 14:05:50 -0400

    update example to set manage_account_alias = false

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,7 +1,8 @@
 module "bootstrap" {
   source = "../../"
 
-  region              = "us-west-2"
-  account_alias       = var.account_alias
-  dynamodb_table_name = var.dynamodb_table_name
+  region               = "us-west-2"
+  account_alias        = var.account_alias
+  dynamodb_table_name  = var.dynamodb_table_name
+  manage_account_alias = false
 }
