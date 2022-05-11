180d060af4acfe7c49611275c078b01f0a646535
Carter Jones <carter@truss.works>
2022-05-11 13:29:25 -0700

    bump module versions for compatibility with aws 4 provider

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_iam_account_alias" "alias" {
 
 module "terraform_state_bucket" {
   source  = "trussworks/s3-private-bucket/aws"
-  version = "~> 3.7.0"
+  version = "~> 4.0.0"
 
   bucket         = local.state_bucket
   logging_bucket = module.terraform_state_bucket_logs.aws_logs_bucket
@@ -31,7 +31,7 @@ module "terraform_state_bucket" {
 
 module "terraform_state_bucket_logs" {
   source  = "trussworks/logs/aws"
-  version = "~> 11.0.0"
+  version = "~> 13.0.0"
 
   s3_bucket_name          = local.logging_bucket
   default_allow           = false
